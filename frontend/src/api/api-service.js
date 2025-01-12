67c2eebba7fb7dfe5a1ca14a29d9e15a7761d9e4
vladdof <vladislav.efanov@gmail.com>
2025-01-12 23:42:26 +0100

    create ApiService

---
+++ frontend/src/api/api-service.js
@@ -0,0 +1,39 @@
+import { API_URL } from '../config';
+
+class ApiService {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    async request(url, options = {}) {
+        try {
+            const response = await fetch(`${this.baseUrl}${url}`, options);
+            return response.json();
+        } catch (error) {
+            console.error(`Ошибка при запросе ${url}: `, error);
+            throw error;
+        }
+    }
+
+    async fetchTasks() {
+        return this.request('/api/tasks');
+    }
+
+    async createTask(text) {
+        return this.request('/api/tasks', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text }),
+        });
+    }
+
+    async deleteTask(id) {
+        return this.request(`/api/tasks/${id}`, {
+            method: 'DELETE',
+        });
+    }
+}
+
+export const apiService = new ApiService(API_URL);
